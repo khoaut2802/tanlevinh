081d7fe5955ea9539e57583bd3883e37f1a5659f
KIEU TRONG THIEN <kietutrongthien@gmail.com>
2021-03-20 10:35:39 +0700

    up

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::resource('categories', 'CategoriesController');
+
+// Route::group(['prefix' => 'categories'], function() {
+//     Route::get('/custom', 'CategoriesController@custom');
+// });
